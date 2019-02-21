389c6ddd09384b2309e67e152ec3943a6e6151d1
Alena-K. Schnurr <alena-kathrin.schnurr@medma.uni-heidelberg.de>
2019-02-21 15:22:50 +0100

    moved config parameters

--- config.py
+++ config.py
@@ -43,17 +43,9 @@ test_size = 1
 summaries_per_case = 10
 
 ##### Data #####
+dtype = tf.float32
 data_train_split = 0.75
 number_of_vald = 2
-dtype = tf.float32
-num_channels = 3
-num_classes_seg = 2
-train_dim = 256
-train_input_shape = [train_dim, train_dim, num_channels]
-train_label_shape = [train_dim, train_dim, num_classes_seg]
-test_dim = 512
-test_data_shape = [test_dim, test_dim, num_channels]
-test_label_shape_seg = [test_dim, test_dim, num_classes_seg]
 
 ##### Loader #####
 vald_reader_instances = 1
@@ -61,11 +53,6 @@ file_name_capacity = 140
 batch_capacity_valid = batch_capacity//2
 
 # Sample Mining
-percent_of_object_samples = 50  # %
-samples_per_slice_liver = 2
-samples_per_slice_lesion = 4
-samples_per_slice_bkg = 1
-samples_per_slice_uni = 1
 patch_shift_factor = 3  # 3*std is 99th percentile
 in_between_slice_factor = 2
 slice_shift = ((num_channels - 1) // 2) * in_between_slice_factor
@@ -83,11 +70,6 @@ target_type_label = sitk.sitkUInt32
 data_background_value = -1000
 label_background_value = 0
 max_rotation = 0.07
-
-# Preprocessing
-norm_min_v = -200
-norm_max_v = 250
-norm_eps = 1e-5
 
 # Tversky
 tversky_alpha = 0.3
