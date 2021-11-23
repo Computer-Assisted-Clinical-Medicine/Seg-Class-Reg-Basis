0e729f5250d5dc291264ac5871bee71db46238f9
Steffen Albert <gitlab@steffenalbert.de>
2021-11-23 16:20:34 +0100

    fixed postprocessing

--- postprocessing.py
+++ postprocessing.py
@@ -39,28 +39,4 @@ def keep_big_structures(input_image: sitk.Image, target_image, n_keep=1):
     processed_image = sitk.GetImageFromArray(input_numpy)
     processed_image.CopyInformation(input_sitk)
 
-    # label all objects (0 is background, rest is foreground)
-    connected_filter = sitk.ConnectedComponentImageFilter()
-    connected_filter.SetFullyConnected(False)
-    connected = connected_filter.Execute(input_sitk)
-
-    # do statistics on components
-    statistics = sitk.LabelShapeStatisticsImageFilter()
-    statistics.Execute(connected)
-
-    n_labels = statistics.GetNumberOfLabels()
-    n_pixels = pd.Series(index=pd.RangeIndex(1, n_labels), dtype=int)
-    for label in range(1, n_labels):
-        n_pixels[label] = statistics.GetNumberOfPixels(label)
-
-    to_remove = n_pixels.sort_values(ascending=False)[n_keep:].index
-    for label in to_remove:
-        input_sitk = sitk.LabelMapMask(
-            labelMapImage=sitk.LabelImageToLabelMap(connected),
-            featureImage=input_sitk,
-            label=label,
-            backgroundValue=0,
-            negated=True,
-        )
-
     sitk.WriteImage(processed_image, str(target_image))
